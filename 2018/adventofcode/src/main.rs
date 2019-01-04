dffc92370f095c28d8c700550188e4165e3ec490
Andrii Mishkovskyi <mishok13@gmail.com>
2019-01-04 10:26:18 +0100

    Day 2 finished

--- 2018/adventofcode/src/main.rs
+++ 2018/adventofcode/src/main.rs
@@ -2,17 +2,18 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
-fn day1part1(path: &Path) {
+fn day1part1(path: &Path) -> Option<String> {
     let file = File::open(&path).unwrap();
     let reader = BufReader::new(file);
     let numbers = reader.lines().map(|line| line.unwrap().parse::<i32>().unwrap());
     let offset = numbers.fold(0, |c, x| c + x);
     println!("Day 1, part 1 answer: {}", offset);
+    Some(offset.to_string())
 }
 
-fn day1part2(path: &Path) {
+fn day1part2(path: &Path) -> Option<String> {
     let file = File::open(&path).unwrap();
     let reader = BufReader::new(file);
     let numbers = reader.lines().map(|line| line.unwrap().parse::<i32>().unwrap()).collect::<Vec<i32>>();
@@ -22,18 +23,102 @@ fn day1part2(path: &Path) {
     for offset in numbers.iter().cycle() {
         current += offset;
         if !seen.insert(current) {
-            println!("Day 1, part 2 answer: {}", current);
-            break
+            return Some(current.to_string())
+        }
+    }
+    None
+}
+
+fn count_chars(string: String) -> HashMap<char, u32> {
+    let mut counter = HashMap::new();
+    for char in string.chars() {
+        let entry = counter.entry(char).or_insert(0);
+        *entry += 1;
+    }
+    counter
+}
+
+fn day2part1(path: &Path) -> Option<String> {
+    let file = File::open(&path).unwrap();
+    let reader = BufReader::new(file);
+    let box_ids = reader
+        .lines()
+        .map(|line| line.unwrap())
+        .map(|line| count_chars(line))
+        .map(|counter| (counter.values().any(|&count| count == 2), counter.values().any(|&count| count == 3)))
+        .fold((0, 0), |collector, matches| {
+            match matches {
+                (true, true) => (collector.0 + 1, collector.1 + 1),
+                (true, false) => (collector.0 + 1, collector.1),
+                (false, true) => (collector.0, collector.1 + 1),
+                (false, false) => collector
+            }});
+    Some((box_ids.0 * box_ids.1).to_string())
+}
+
+fn unique_subwords(word: String) -> impl Iterator<Item = String> {
+    let mut subwords = HashSet::new();
+    for index in 0..word.chars().count() {
+        let mut subword = word.clone();
+        subword.remove(index);
+        subwords.insert(subword);
+    }
+    subwords.into_iter()
+}
+
+fn day2part2(path: &Path) -> Option<String> {
+    let file = File::open(&path).unwrap();
+    let reader = BufReader::new(file);
+    let subwords: Vec<String> = reader.lines().map(|line| line.unwrap()).flat_map(unique_subwords).collect();
+    let mut seen = HashSet::new();
+    for word in subwords.iter() {
+        if !seen.insert(word.clone()) {
+            return Some(word.to_string())
+        }
+    }
+    None
+}
+
+struct Solution<'a> {
+    path: &'a Path,
+    day: u8,
+    part: u8,
+    solver: fn(&Path) -> Option<String>
+}
+
+impl<'a> Solution<'a> {
+    fn new(path: &'a str, day: u8, part: u8, solver: fn(&Path) -> Option<String>) -> Solution<'a> {
+        Solution{path: &Path::new(path),
+                 day: day,
+                 part: part,
+                 solver: solver}
+    }
+
+    fn solve(self) {
+        if let Some(answer) = (self.solver)(self.path) {
+            println!("Day: {}, part: {}, answer: {}", self.day, self.part, answer)
+        } else {
+            println!("Day: {}, part: {}, no solution found", self.day, self.part)
         }
     }
 }
 
-fn day2(path: &Path) {
-
-}
-
 fn main()  {
-    let path = Path::new("00.txt");
-    day1part1(path);
-    day1part2(path);
+    // for day, part, path, solver
+    // open path, pass to solver
+    // print out returned result
+    let solutions = vec![
+        Solution::new("00.txt", 1, 1, day1part1),
+        Solution::new("00.txt", 1, 2, day1part2),
+        Solution::new("01.txt", 1, 1, day2part1),
+        Solution::new("01.txt", 1, 2, day2part2),
+    ];
+    for solution in solutions {
+        solution.solve()
+    }
+    // let path = Path::new("00.txt");
+    // day1part1(path);
+    // day1part2(path);
+    // day2part1(Path::new("01.txt"));
+    // day2part2(Path::new("01.txt"));
 }
